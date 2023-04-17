68576a01bdf74b5418e472442b18fdece9fa54b0
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-04-17 09:46:48 +0000

    Revert to Python3.9

--- flake.nix
+++ flake.nix
@@ -47,7 +47,7 @@
                 isort.enable = true;
               };
               packages = [
-                (pkgs.python310.withPackages (ps: with ps; [
+                (pkgs.python39.withPackages (ps: with ps; [
                   pip
                   isort
                   black
